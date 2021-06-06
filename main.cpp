464ead0945e1b7ec158392cb8bffa0b036c3ad7d
Janix4000 <janix4000@gmail.com>
2021-06-06 21:33:51 +0200

    Changed Document window size

--- main.cpp
+++ main.cpp
@@ -19,8 +19,12 @@ int w = 420, h = 596;
 
 int main() {
 	cv::VideoCapture cap(0);
+	
 	cv::Mat img;
 	cv::Mat doc_img;
+	const int height = cap.get(cv::CAP_PROP_FRAME_HEIGHT);
+	const int width = static_cast<int>(float(height) / 1.41);
+
 
 	DocumentCropper<DocumentViaColorPreprocessor> document_cropper;
 	//DocumentCropper<DocumentGrayScalePreprocessor> document_cropper;
@@ -30,10 +34,11 @@ int main() {
 
 		cv::imshow("Image", img);
 		if (document_cropper.is_document_found()) {
-			document_cropper.get_cropped_document(doc_img, { 600, 900 });
+			document_cropper.get_cropped_document(doc_img, cv::Size{ 600, 900 });
+			cv::cvtColor(doc_img, doc_img, cv::COLOR_BGR2GRAY);
 			cv::imshow("Document", doc_img);
 		}
-		document_cropper.show_preprocess();
+		//document_cropper.show_preprocess();
 
 		cv::waitKey(1);
 	}
