0b480464e18d14193e2a0a24a3db45d591053e24
Janix4000 <janix4000@gmail.com>
2021-07-26 13:01:31 +0200

    move libs to airly_libs dir

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-from FilesManager import __version__
+from airly_libs.FilesManager import __version__
 
 setup(
     name='airly_libs',
@@ -12,6 +12,13 @@ setup(
 
     py_modules=find_packages(),
     install_requires=[
-
+        'autopep8>=1.5.7',
+        'certifi>=2021.5.30',
+        'charset-normalizer>=2.0.3',
+        'idna>=3.2',
+        'pycodestyle>=2.7.0',
+        'requests>=2.26.0',
+        'toml>=0.10.2',
+        'urllib3>=1.26.6',
     ],
 )
